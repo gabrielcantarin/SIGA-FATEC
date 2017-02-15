5bc5cb5eee64724b541044dc5f34aa34f479fff8
Gabriel Simões <gabrielsimoes@Gabriels-MacBook-Pro.local>
2017-02-15 19:45:14 -0200

    correcao

--- inject/batbook.js
+++ inject/batbook.js
@@ -1,41 +1,41 @@
 
 
 $(document).ready(function() {
-	// alert('Bem-vindo Professor! Essa extensão foi criada pelo Gabriel Cantarin para tentar ajudar no seu dia a dia :)');
 
+	professoresPresencaFalta();
 	
-	
-	setTimeout(function(){
 
-		var table = document.getElementById("Grid1ContainerTbl");
-		var table = $(table);
-		console.log(table.find('tbody > tr').append('<td><input type="checkbox">  <input type="checkbox">  <input type="checkbox"></td>'));
-		console.log(table.find('tbody > tr').append('<td><a href="#" name="presenca">Presença</a>  |  <a href="#" name="falta">Falta</a></td>'));
+	function professoresPresencaFalta(){
 
-		$("[name='presenca']").click(function(){
-			// console.log($(this).parent().parent().find('td'));
-			// console.log($(this).parent().parent().find('td').eq(7));
-			// console.log($(this).parent().parent().find('td').eq(7).find('input'));
-			$(this).parent().parent().find('td').eq(7).find('input').each(function(){
-				$(this).prop( "checked", true );
-			});
-		});
+		var URL = window.location.href.split('/').reverse()[0].split('.')[0];
+		var COLUNA_COM_CHECK_LIST = 8;
+		var ID_TABELA_PRINCIPAL = "Grid1ContainerTbl";
 
-		$("[name='falta']").click(function(){
-			// console.log($(this).parent().parent().find('td'));
-			// console.log($(this).parent().parent().find('td').eq(7));
-			// console.log($(this).parent().parent().find('td').eq(7).find('input'));
-			$(this).parent().parent().find('td').eq(7).find('input').each(function(){
-				$(this).prop( "checked", false );
-			});
-		});
+		if (URL == "historicocompleto"){
+			setTimeout(function(){
+
+				var table = document.getElementById(ID_TABELA_PRINCIPAL);
+				var table = $(table);
+				table.find('tbody > tr').append('<td><input type="checkbox">  <input type="checkbox">  <input type="checkbox"></td>');
+				table.find('tbody > tr').append('<td><a href="#" name="presenca">Presença</a>  |  <a href="#" name="falta">Falta</a></td>');
+
+				$("[name='presenca']").click(function(){
+					$(this).parent().parent().find('td').eq(COLUNA_COM_CHECK_LIST).find('input').each(function(){
+						$(this).prop( "checked", true );
+					});
+				});
+
+				$("[name='falta']").click(function(){
+					$(this).parent().parent().find('td').eq(COLUNA_COM_CHECK_LIST).find('input').each(function(){
+						$(this).prop( "checked", false );
+					});
+				});
 
 
-	}, 500);
-
-	
-	
-	
+			}, 500);
+		}
+		
+	}
 });
 
 
